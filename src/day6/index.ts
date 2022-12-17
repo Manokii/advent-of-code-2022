e87e4c597c557318f05b63b7ea8e6ed975baad70
Jasper Concepcion <jasper@manok.dev>
2022-12-17 20:10:09 +0800

    add day6

---
+++ src/day6/index.ts
@@ -0,0 +1,39 @@
+import { readInput } from "utils/readInput"
+import { writeAnswer } from "utils/writeAnswer"
+
+const getSignal = (input: string, characterCount: number) => {
+  let signalMarker = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const chars = input.slice(i, i + characterCount)
+    if (!isAllUnique(chars)) return
+    signalMarker = i + characterCount
+    break
+  }
+
+  return signalMarker
+}
+
+function isAllUnique(str: string) {
+  return !/(.).*\1/.test(str)
+}
+
+function getAnswer(input: string[], characterCount: number) {
+  return input.map((line) => getSignal(line, characterCount))
+}
+// ===================================
+async function solution(inputPath: string, name: string) {
+  const input = await readInput(inputPath, false)
+  const partOne = getAnswer(input.lines, 4)
+  const partTwo = getAnswer(input.lines, 14)
+  await writeAnswer(partOne, name, 1)
+  await writeAnswer(partTwo, name, 2)
+}
+
+const INPUT_FILE_SAMPLE = "./src/day6/input-sample.txt"
+const INPUT_FILE = "./src/day6/input.txt"
+
+export function day6() {
+  SAMPLE: solution(INPUT_FILE_SAMPLE, "day6-sample")
+  MAIN: solution(INPUT_FILE, "day6")
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { day2 } from "./day2"
 import { day3 } from "./day3"
 import { day4 } from "./day4"
 import { day5 } from "./day5"
+import { day6 } from "./day6"
 
 async function main() {
   day1()
@@ -10,6 +11,7 @@ async function main() {
   day3()
   day4()
   day5()
+  day6()
 }
 
 main().then()
